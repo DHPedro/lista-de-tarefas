d4adfb2941e133f1993ebda341cec9e6c4173b04
DH_Pedro <98625506+DHPedro@users.noreply.github.com>
2023-09-29 14:49:19 -0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,11 +1,11 @@
 // Recupere as variáveis de ambiente secretas do GitHub usando `process.env`
-const apiKey = secrets.APIKEY;
-const authDomain = secrets.AUTHDOMAIN;
-const databaseURL = secrets.DATABASEURL;
-const projectId = secrets.PROJECTID;
-const storageBucket = secrets.STORAGEBUCKET;
-const messagingSenderId = secrets.MESSAGINGSENDERID;
-const appId = secrets.APPID;
+const apiKey = ${{ secrets.API_KEY }};
+const authDomain = ${{ secrets.AUTHDOMAIN }};
+const databaseURL = ${{ secrets.DATABASEURL}};
+const projectId = ${{ secrets.PROJECTID }};
+const storageBucket = ${{ secrets.STORAGEBUCKET }};
+const messagingSenderId = ${{ secrets.MESSAGINGSENDERID }};
+const appId = ${{ secrets.APPID }};
 
 // Configure o Firebase com as informações recuperadas
 const firebaseConfig = {
