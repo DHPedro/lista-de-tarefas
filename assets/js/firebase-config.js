7456210456edab7bf1cd40a2366876f30ed1fa04
DH_Pedro <98625506+DHPedro@users.noreply.github.com>
2023-11-04 20:03:19 +0000

    commit

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,12 +1,11 @@
-// Configure o Firebase com as informações recuperadas
 const firebaseConfig = {
-  apiKey: "AIzaSyDluoFXPm-M4CmSv-Qc4zlbKZ97WBmtbTA",
-  authDomain: "tarefas-55cab.firebaseapp.com",
-  databaseURL: "https://tarefas-55cab-default-rtdb.firebaseio.com",
-  projectId: "tarefas-55cab",
-  storageBucket: "tarefas-55cab.appspot.com",
-  messagingSenderId: "621664361214",
-  appId: "1:621664361214:web:f1dbadb95b7a632d87a3af",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
 };
 
 firebase.initializeApp(firebaseConfig);
